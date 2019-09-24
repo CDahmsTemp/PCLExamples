0f7361674c2af8dc8b848f19c56130a621d421c0
Chris Dahms <cdahms123@gmail.com>
2019-09-24 07:27:57 -0400

    update

--- WriteCloud.cpp
+++ WriteCloud.cpp
@@ -9,10 +9,11 @@ int main(void)
 {
   std::cout << "\n\n" << "starting program" << "\n\n";
 
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+  // instantiate a cloud
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
+  // populate the cloud with 20 random points
   int numPoints = 20;
-
   for (int i = 0; i < numPoints; i++)
   {
     pcl::PointXYZ point;
@@ -21,18 +22,21 @@ int main(void)
     point.y = 1024 * rand() / (RAND_MAX + 1.0f);
     point.z = 1024 * rand() / (RAND_MAX + 1.0f);
 
-    cloud.points.push_back(point);
+    cloud->points.push_back(point);
   }
 
-  // for simplicity, use an "unorganized" cloud, "width" = num points, "height" = 1
-  cloud.width = (int)cloud.points.size();
-  cloud.height = 1;
+  // for simplicity, use and unorganized cloud, i.e. width = num points, height = 1
+  cloud->width = (int)cloud->points.size();
+  cloud->height = 1;
 
-  pcl::io::savePCDFileASCII("my_cloud.pcd", cloud);
+  // write the cloud to file
+  pcl::io::savePCDFileASCII("my_cloud.pcd", *cloud);
 
   std::cout << "\n\n" << "program complete" << "\n\n";
 
   return (0);
 }
+
+
 
 
